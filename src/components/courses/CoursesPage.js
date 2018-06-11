762d54eea644c8a5a31c390b1d2bc5fce8866db2
lyas15u282 <jacoblutin@gmail.com>
2018-06-11 11:39:35 +0300

    courses page simple react form added

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    
+    this.state = {
+      course: { title: '' }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave() {
+    alert('Saving ' + this.state.course.title);
+  }
+
   render() {
     return (
       <div>
-        <h1>Courses</h1>
+        <div className="py-4">
+          <h1>Courses</h1>
+        </div>
+        <div className="col-md-6 order-md-1">
+          <h4 className="mb-3">Add course:</h4>
+          <form>
+            <div className="form-group">
+              <label forHtml="title">Title</label>
+              <input type="text" onChange={this.onTitleChange}
+                     name="title" value={this.state.course.title}
+                     className="form-control"/>
+            </div>
+            <input type="submit" className="btn btn-primary"
+                   value="Save" onClick={this.onClickSave}/>
+          </form>
+        </div>
       </div>
     );
   }
