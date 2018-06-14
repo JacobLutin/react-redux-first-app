dc6058de7b1b8e5320497d7962c06ea8bd9230e5
lyas15u282 <jacoblutin@gmail.com>
2018-06-14 13:48:57 +0300

    loading courses from api action & reducer implemented

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,9 +3,9 @@ import * as types from "../actions/actionTypes";
 export default function courseReducer(state = [], action) {
   switch(action.type) {
     case types.CREATE_COURSE:
-      // state.push(action.course);
-      // return state;
       return [...state, Object.assign({}, action.course)];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
